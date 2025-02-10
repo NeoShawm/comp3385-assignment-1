76493c1b7d57dffac6c65057e8b47dd1fb653c88
Naelon George <georgenaelon@gmail.com>
2025-02-09 21:19:45 -0400

    Created AboutController and view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\AboutController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/about', function() {
     return "My About Page";
 });
+
+Route::get('/about', [AboutController::class, 'show']);
